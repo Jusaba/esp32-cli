75fb82caba81be3111a98ea05d1b49a336deb950
Julian Salas Bartolome <jsbpublico@gmail.com>
2025-02-11 18:45:47 +0100

    Actualizar los flags de compilación para el núcleo esp32 en el script Compila.sh

--- Compila.sh
+++ Compila.sh
@@ -2,7 +2,7 @@
   mv /home/bin/esp32/"${1}".ino /home/bin/esp32/esp32.ino
   case $2 in
 	  "esp32:esp32:stamp-s3")   	
-               arduino-cli compile -b esp32:esp32:stamp-s3  --build-property  " build.extra_flags= -DESP32"  -e -vls
+               arduino-cli compile -b esp32:esp32:stamp-s3  --build-property  " build.extra_flags= -D ESP32 -D ARDUINO_USB_MODE=1 -D ARDUINO_USB_CDC_ON_BOOT=1"  -e -vls
           ;;
   esac
   mv /home/bin/esp32/esp32.ino /home/bin/esp32/"${1}".ino    
