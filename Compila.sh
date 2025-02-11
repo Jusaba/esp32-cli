6671cfe22644a6c350645b402915e836cca811db
Julian Salas Bartolome <jsbpublico@gmail.com>
2025-02-11 20:36:38 +0100

    Prueba

--- Compila.sh
+++ Compila.sh
@@ -6,7 +6,7 @@
   ls
   case $2 in
 	  "esp32:esp32:stamp-s3")   	
-               arduino-cli compile -b esp32:esp32:stamp-s3  --build-property  " build.extra_flags= -D ESP32 -D ARDUINO_USB_MODE=1 -D ARDUINO_USB_CDC_ON_BOOT=1"  -e -vls
+               arduino-cli compile -b esp32:esp32:stamp-s3  --build-property  " build.extra_flags= -D ESP32 -D ARDUINO_USB_MODE=1 -D ARDUINO_USB_CDC_ON_BOOT=1"  --output-dir /home/bin/esp32 -e -vls
           ;;
   esac
   echo "-3-"
