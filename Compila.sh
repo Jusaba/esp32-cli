91694696ef03e21729ccf8d08bed556506bdde84
Julian Salas Bartolome <jsbpublico@gmail.com>
2025-02-11 17:29:56 +0100

    Actualizar el script de compilación para incluir flags adicionales en la compilación del núcleo esp32

--- Compila.sh
+++ Compila.sh
@@ -2,7 +2,7 @@
   mv /home/bin/esp32/"${1}".ino /home/bin/esp32/esp32.ino
   case $2 in
 	  "esp32:esp32:stamp-s3")   	
-               arduino-cli compile --output-dir . --fqbn esp32:esp32:stamp-s3 -e -vls
+               arduino-cli compile -b esp32:esp32:stamp-s3  --build-property  " build.extra_flags= -DESP32"  -e -vls
           ;;
   esac
   mv /home/bin/esp32/esp32.ino /home/bin/esp32/"${1}".ino    
